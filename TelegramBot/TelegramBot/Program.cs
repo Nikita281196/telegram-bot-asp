3efd3e8c2f324396c5fc25fe60acaf7cab2707ca
Nikita <nikitagal96@gmail.com>
2023-07-29 20:40:23 +0700

    initial

---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace TelegramBot
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var token = ConfigurationManager.AppSettings["Token"];
+
+            var bot = new BotService(token);
+            BotService.Start();
+        }
+    }
+}
